8a5a6a00e8d90c912c82bda8ee268cc460dc45b8
nipun0607 <nipunmadan0607@gmail.com>
2024-01-17 23:07:35 +0530

    created

---
+++ src/Components/ContactUs/Contact.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Contact.css'
+
+const Contact = () => {
+  return (
+    <main>
+      <section className="contact">
+      <div className="container">
+        <h1>Contact Me</h1>
+        <p>Created by :Nipun Madan</p>
+        <p>Email Address : nipunmadan0607@gmail.com</p>
+       
+      </div>
+    </section>
+    </main>
+  )
+}
+
+export default Contact
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import './Footer.css'
+import React from 'react'
+
+const Footer = () => {
+  console.log("Footer");
+  return (
+    <footer>
+        Copyright@2024 | All Rights Reserved | by <a href="https://github.com/nipun0607?tab=repositories" target="_blank" rel="noopener noreferrer">Nipun Madan</a>
+    </footer>
+  )
+}
+
+export default Footer
